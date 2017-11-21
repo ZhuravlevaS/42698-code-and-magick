4fc5e4bf1258b026eb3f2764e8c180c63cdd8296
Zhuravleva Svetlana <szhuravleva@ws77-005.local>
2017-11-21 16:49:27 +0300

    добавила объект по типу

--- js/stat.js
+++ js/stat.js
@@ -5,42 +5,48 @@ window.renderStatistics = function (ctx, names, times) {
   var CloudParams = {
     width: 420, // px
     height: 270, // px
+    // left: 100, // px
+    // top: 10, // px
+    // indentShadow: 10 // px
+  };
+
+  var Indents = {
+    histogramIndent: 50,
     left: 100, // px
     top: 10, // px
-    indentShadow: 10 // px
+    indentShadow: 10, // px
+    textIndent: 10 // px
   };
 
   var HistogramParams = {
     height: 150, // px
     columnWidth: 40, // px
-    indent: 50 // px
   };
 
   var textParams = {
     fontSize: 16, // px
     fontFamily: 'PT Mono',
-    indent: 10 // px
   };
 
   var step = HistogramParams.height / Math.max.apply(null, times); // px
   var indent = 50; // px
-  var initialX = CloudParams.left + indent; // px;
-  var initialY = CloudParams.height - CloudParams.indentShadow - textParams.indent; // px
+  var initialX = Indents.left + indent; // px;
+  var initialY = CloudParams.height - Indents.indentShadow - Indents.textIndent; // px
 
   // Рисуем тень облака
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(CloudParams.left + CloudParams.indentShadow, CloudParams.top + CloudParams.indentShadow, CloudParams.width, CloudParams.height);
+  ctx.fillRect(Indents.left + Indents.indentShadow, Indents.top + Indents.indentShadow, CloudParams.width, CloudParams.height);
 
   // Рисуем само облако
   ctx.fillStyle = 'white';
-  ctx.fillRect(CloudParams.left, CloudParams.top, CloudParams.width, CloudParams.height);
+  ctx.fillRect(Indents.left, Indents.top, CloudParams.width, CloudParams.height);
 
   // Вставляем текст
   ctx.fillStyle = '#000';
   ctx.font = textParams.fontSize + 'px' + textParams.fontFamily;
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CloudParams.left + indent, CloudParams.top + textParams.indent);
-  ctx.fillText('Список результатов:', CloudParams.left + indent, CloudParams.top + textParams.indent + textParams.fontSize);
+  ctx.fillText('Ура вы победили!', Indents.left + indent, Indents.top + Indents.textIndent);
+  ctx.fillText('Список результатов:', Indents.left + indent, Indents.top + Indents.textIndent + textParams.fontSize);
 
   var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -54,8 +60,8 @@ window.renderStatistics = function (ctx, names, times) {
 
     ctx.fillStyle = '#000';
     // Вставляем имя и время прохождения
-    ctx.fillText(name, initialX + (HistogramParams.columnWidth + HistogramParams.indent) * i, initialY + textParams.indent);
-    ctx.fillText(time.toFixed(), initialX + (HistogramParams.columnWidth + HistogramParams.indent) * i, initialY - time * step - textParams.indent * 2);
+    ctx.fillText(name, initialX + (HistogramParams.columnWidth + Indents.histogramIndent) * i, initialY + Indents.textIndent);
+    ctx.fillText(time.toFixed(), initialX + (HistogramParams.columnWidth + Indents.histogramIndent) * i, initialY - time * step - Indents.textIndent * 2);
   };
 
   times.forEach(function (time, i) {
