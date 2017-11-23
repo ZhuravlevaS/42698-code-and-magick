e01d3aff38b29e91a69c47ffb664930ea491666b
Zhuravleva Svetlana <szhuravleva@ws77-005.local>
2017-11-23 16:43:00 +0300

    fix bugs

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,5 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
-
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -9,48 +7,39 @@ var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+var wizardQuantity = 4;
 
 var getRandomName = function () {
-  var Randomname = names[Math.floor(Math.random() * names.length)];
-  var Randomsurname = surnames[Math.floor(Math.random() * surnames.length)];
-  var name = Randomname + ' ' + Randomsurname;
+  var randomName = names[Math.floor(Math.random() * names.length)];
+  var randomSurname = surnames[Math.floor(Math.random() * surnames.length)];
+  var name = randomName + ' ' + randomSurname;
   return name;
 };
 
 var getRandomCoatColor = function () {
-  var RandomcoatColor = coatColors[Math.floor(Math.random() * coatColors.length)];
-  var coatColor = RandomcoatColor;
+  var randomCoatColor = coatColors[Math.floor(Math.random() * coatColors.length)];
+  var coatColor = randomCoatColor;
   return coatColor;
 };
 
 var getRandomEyesColor = function () {
-  var RandomeyesColor = eyesColors[Math.floor(Math.random() * eyesColors.length)];
-  var eyesColor = RandomeyesColor;
+  var randomEyesColor = eyesColors[Math.floor(Math.random() * eyesColors.length)];
+  var eyesColor = randomEyesColor;
   return eyesColor;
 };
 
-var wizards = [
-  {
+var wizards = [];
+
+for (var k = 0; k < wizardQuantity; k++) {
+
+  var wizardParams = {
     name: getRandomName(),
     coatColor: getRandomCoatColor(),
     eyesColor: getRandomEyesColor()
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor()
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor()
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor()
-  }
-];
+  };
+
+  wizards.push(wizardParams);
+}
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -63,9 +52,14 @@ var renderWizard = function (wizard) {
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < wizards.length; i++) {
+var drawWizards = function () {
   fragment.appendChild(renderWizard(wizards[i]));
+};
+
+for (var i = 0; i < wizards.length; i++) {
+  drawWizards();
 }
 similarListElement.appendChild(fragment);
 
+document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
